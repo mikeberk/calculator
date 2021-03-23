a731e3ee416e2cdad33ce09a19b67b6b0cf417c6
Berk <mikeberk93@gmail.com>
2021-03-22 22:59:07 -0500

    Created operate function and started initial UI setup

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+function operate (operator, a, b) {
+    switch (operator) {
+        case "+":
+            return add(a, b);
+            break;
+        case "-":
+            return subtract(a, b);
+            break;
+        case "*":
+            return multiply(a, b);
+            break;
+        case "/":
+            return divide(a, b);
+            break;
+    }
+}
